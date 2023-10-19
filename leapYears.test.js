77246b7b320b05f1d57daa8d43ae9ffdad96ec71
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-19 11:29:44 +0100

    test: returns true for number divisible by 400

--- leapYears.test.js
+++ leapYears.test.js
@@ -12,4 +12,8 @@ describe("leapYears", () => {
   it("returns false for 500", () => {
     expect(leapYears(500)).toBe(false);
   });
+
+  it("returns true for number divisible by 400", () => {
+    expect(leapYears(2000)).toBe(true);
+  });
 });
