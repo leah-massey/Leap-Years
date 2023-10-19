5e316e2a3141dd95984fa3d49a28dc4809f3cf0b
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-19 11:37:39 +0100

    refactor

--- leapYears.test.js
+++ leapYears.test.js
@@ -5,11 +5,11 @@ describe("leapYears", () => {
     expect(typeof leapYears(400) === "boolean").toBeTruthy();
   });
 
-  it("returns false for 500", () => {
-    expect(leapYears(500)).toBe(false);
-  });
-
   it("returns true for number divisible by 400", () => {
     expect(leapYears(2000)).toBe(true);
+  });
+
+  it("returns false for numbers not divisible by 400", () => {
+    expect(leapYears(700)).toBe(false);
   });
 });
