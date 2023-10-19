a95a1cb52efb9e58a85a60dbefcad20d732fc3e6
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-10-19 11:17:09 +0100

    test(hardcoded): returns true for 400

--- leapYears.test.js
+++ leapYears.test.js
@@ -4,4 +4,8 @@ describe("leapYears", () => {
   it("returns a boolean value", () => {
     expect(typeof leapYears(400) === "boolean").toBeTruthy();
   });
+
+  it("returns true for 400", () => {
+    expect(leapYears(400)).toBe(true);
+  });
 });
